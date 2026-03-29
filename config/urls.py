d9714e05195cdfd9b827884f40d249578c975e62
truongthihonghoa <truongthihonghoa.qnm22@gmail.com>
2026-03-29 09:31:25 +0700

    update UI + images

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='dashboard', permanent=False)),
     path('admin/', admin.site.urls),
     path('accounts/', include('apps.accounts.urls')),
     path('branches/', include('apps.branches.urls')),
