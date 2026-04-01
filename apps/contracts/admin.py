3e05401b4611df0b8c68962f592567624c2efe87
ThuTrang2512 <nguyenthithutrang.tienphuoc1@gmail.com>
2026-04-01 18:16:47 +0700

    update hop dong

--- apps/contracts/admin.py
+++ apps/contracts/admin.py
@@ -1,18 +1,35 @@
 from django.contrib import admin
+
 from .models import HopDongLaoDong, HopDongLD_CT
-# Register your models here.
+
 
 @admin.register(HopDongLaoDong)
 class HopDongLaoDongAdmin(admin.ModelAdmin):
-    list_display = ('ma_hd', 'ma_nv', 'ten_nv', 'loai_hd', 'ngay_bd', 'ngay_kt', 'chuc_vu', 'trang_thai')
-    search_fields = ('ma_hd', 'ma_nv', 'ten_nv')
-    list_filter = ('loai_hd', 'trang_thai')
-    raw_id_fields = ('ma_nv',)
-    autocomplete_fields = ('ma_nv',)
+    list_display = (
+        'ma_hd',
+        'ma_nv',
+        'ma_chi_nhanh',
+        'loai_hd',
+        'chuc_vu',
+        'ngay_bat_dau',
+        'ngay_ket_thuc',
+        'trang_thai',
+    )
+    search_fields = ('ma_hd', 'ma_nv__ma_nv', 'ma_nv__ho_ten')
+    list_filter = ('loai_hd', 'chuc_vu', 'trang_thai')
+    raw_id_fields = ('ma_nv', 'ma_chi_nhanh')
+    autocomplete_fields = ('ma_nv', 'ma_chi_nhanh')
+
 
 @admin.register(HopDongLD_CT)
 class HopDongLD_CTAdmin(admin.ModelAdmin):
-    list_display = ('ma_hd', 'luong_co_ban', 'luong_theo_gio', 'so_gio_lam', 'thuong', 'phat')
-    search_fields = ('ma_hd',)
+    list_display = (
+        'ma_hd',
+        'luong_co_ban',
+        'luong_theo_gio',
+        'so_gio_lam',
+        'che_do_thuong',
+    )
+    search_fields = ('ma_hd__ma_hd',)
     raw_id_fields = ('ma_hd',)
     autocomplete_fields = ('ma_hd',)
--- apps/schedules/models.py
+++ apps/schedules/models.py
@@ -1,6 +1,7 @@
-# apps/schedules/models.py
-
 from django.db import models
+
+from apps.employees.models import NhanVien
+
 
 class LichLamViec(models.Model):
     ma_llv = models.CharField(max_length=20, primary_key=True)
@@ -20,6 +21,7 @@ class LichLamViec(models.Model):
     ca_lam = models.CharField(max_length=50)
     trang_thai = models.CharField(max_length=50)
     ngay_tao = models.DateField()
+<<<<<<< HEAD
     ghi_chu = models.TextField(blank=True, null=True)
 
     class Meta:
@@ -29,3 +31,20 @@ class LichLamViec(models.Model):
 
     def __str__(self):
         return f"{self.ma_llv} - {self.ma_nv}"
+=======
+
+    def __str__(self):
+        return self.ma_llv
+
+
+class LichLamViec_CT(models.Model):
+    ma_llv = models.ForeignKey(LichLamViec, on_delete=models.CASCADE)
+    ma_nv = models.ForeignKey(NhanVien, on_delete=models.CASCADE)
+    vi_tri_vl = models.CharField(max_length=100)
+
+    class Meta:
+        unique_together = ('ma_llv', 'ma_nv')
+
+    def __str__(self):
+        return f'{self.ma_llv_id} - {self.ma_nv_id}'
+>>>>>>> 6c4e737 (Hop dong)
